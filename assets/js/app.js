eaa9dbe353a28c5cdc4d81967492b0344eb15335
Thinh Nguyen <ntthinh@cusc.ctu.edu.vn>
2021-05-24 16:55:49 +0900

    up

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,23 @@
 
 (function () {
 
-    $("#lunrsearchresults").on('click', '#btnx', function () {
-        $('#lunrsearchresults').hide( 1000 );
-        $( "body" ).removeClass( "modal-open" );
-    });
+  $("#lunrsearchresults").on('click', '#btnx', function () {
+    $('#lunrsearchresults').hide(1000);
+    $("body").removeClass("modal-open");
+  });
 
   console.log(`%c
   Contact :
   E: thanhthinhcntt@gmail.com
   `, 'background: #4caf50; color: #fff; font-size: 32px;');
+
+  var options = {
+    classname: 'topnav',
+    id: 'MagicMenu'
+  };
+  var nanobar = new Nanobar(options);
+  nanobar.go(30);
+  nanobar.go(76);
+  nanobar.go(100);
+
 })();
